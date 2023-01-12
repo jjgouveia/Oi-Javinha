3c223d89750ff01481ff0e0f9696b2d914aaa74d
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-12 01:45:57 -0300

    Solved

---
+++ src/edu/hackerRank/JavaLoops1.java
@@ -0,0 +1,19 @@
+package edu.hackerRank;
+
+//Tabuada
+
+import java.io.*;
+
+public class JavaLoops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
