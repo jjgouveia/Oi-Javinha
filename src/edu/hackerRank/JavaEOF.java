1a671910aa69c9b0cba064f09ab052385a060c0e
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-15 18:37:16 -0300

    Solved

---
+++ src/edu/hackerRank/JavaEOF.java
@@ -0,0 +1,19 @@
+package edu.hackerRank;
+
+import java.util.Scanner;
+
+public class JavaEOF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int i = 1;
+            while(sc.hasNext()){
+                System.out.println(i+" "+sc.nextLine());
+                i++;
+            }
+            
+            sc.close();
+    
+        }
+    }
+
