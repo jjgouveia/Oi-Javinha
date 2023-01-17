9e4c950c283e513a812687d6c9d680425549a61d
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-16 23:59:47 -0300

    Projetinho rápido

---
+++ src/edu/aulas/controleDeFluxo/DiasDaSemanaSwitch.java
@@ -0,0 +1,45 @@
+package edu.aulas.controleDeFluxo;
+
+import java.util.Scanner;
+
+public class DiasDaSemanaSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número de 1 a 7:");
+        int dia = sc.nextInt();
+        sc.close();
+
+        String diaString = "";
+        
+        switch (dia) {
+            case 1:
+                diaString = "Domingo";
+                break;
+            case 2:
+                diaString = "Segunda";
+                break;
+            case 3:
+                diaString = "Terça";
+                break;
+            case 4:
+                diaString = "Quarta";
+                break;
+            case 5:
+                diaString = "Quinta";
+                break;
+            case 6:
+                diaString = "Sexta";
+                break;
+            case 7:
+                diaString = "Sábado";
+                break;
+            default:
+            System.out.println("Digite SOMENTE um valor entre 1 e 7");
+                break;
+        }
+
+        System.out.println(diaString);
+    }
+
+    
+}
--- src/edu/hackerRank/JavaEOF.java
+++ src/edu/hackerRank/JavaEOF.java
@@ -3,7 +3,19 @@ package edu.hackerRank;
 import java.util.Scanner;
 
 public class JavaEOF {
+
+          /***
+         * Este método itera uma contagem
+         * de acordo com a quantidade de vezes
+         * que é recebido algo na entrada.
+         * Uma vez que a entrada seja vazia,
+         * o código para.
+         * 
+         */
+        
     public static void main(String[] args) {
+
+  
         Scanner sc = new Scanner(System.in);
     
         int i = 1;
