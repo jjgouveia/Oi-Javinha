7dd1a7ebde335814768aa607a05307354befe661
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-18 17:12:07 -0300

    Java Currecy Formatter

---
+++ src/edu/hackerRank/JavaCurrencyFormatter.java
@@ -0,0 +1,26 @@
+package edu.hackerRank;
+
+import java.util.*;
+import java.text.*;
+
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        double payment = scanner.nextDouble();
+        scanner.close();
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        // Write your code here.
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+
+}
