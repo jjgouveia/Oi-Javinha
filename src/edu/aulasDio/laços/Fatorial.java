d869f92ac808fcb73c4cee74565cf6661ab47d27
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-17 23:01:37 -0300

    Ex02 for - improved

--- src/edu/aulasDio/laços/Fatorial.java
+++ src/edu/aulasDio/laços/Fatorial.java
@@ -9,11 +9,17 @@ public class Fatorial {
         int fatorial = sc.nextInt();
         System.out.println("----------------------------------");
         int result = 1;
-        
+
+        while (fatorial < 0) {
+            System.out.println("Insira somente números positivos!");
+            System.out.println("Digite um valor válido: ");
+            fatorial = sc.nextInt();
+        }
+
         for (int i = fatorial; i > 1; i--) {
             result *= i;
         }
         sc.close();
-        System.out.println(fatorial+ "! é igual a: " + result + ".");
+        System.out.println(fatorial + "! é igual a: " + result + ".");
     }
 }
